b8a172b26f23fdf59d49aa3b15b32c8fb5f60f3c
ajay3051 <ajaykumarstudymail@gmail.com>
2026-02-07 13:00:01 +0530

    Revert "EvenAndOdd code completed"
    This reverts commit 6e13318f52423b679ad1b4a1153e2408cdc9dc91.

--- EvenAndOdd.java
+++ EvenAndOdd.java
@@ -1,12 +1,5 @@
 package javaProject;
 
 public class EvenAndOdd {
-  public static int num = 100;
-  public static void main(String[] args){
-    if(num % 2 == 0){
-      System.out.println("Even Number HAHAHA");
-    }else{
-      System.out.println("Odd Number UMMMMMMM");
-    }
-  }
+  
 }

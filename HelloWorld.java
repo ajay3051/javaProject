e3735ac4f48b319dcd4271710e91d45d0731925d
ajay3051 <ajaykumarstudymail@gmail.com>
2026-02-09 11:42:17 +0530

    Video call feature added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,19 @@
 package javaProject;
 
 public class HelloWorld {
-  public void audioCall(){
+  public void audioCall() {
     System.out.println("This is audio call method");
     System.out.println("Audio Call Version 5.1");
+  }
+
+  public void videoCall() {
+    System.out.println("This is video call method");
+    System.out.println("Video call version 2.1");
   }
   public static void main(String[] args){
     HelloWorld hw = new HelloWorld();
     hw.audioCall();
+    hw.videoCall();
     System.out.println("Hello World###");
     System.out.println("Thank You!");
   }
